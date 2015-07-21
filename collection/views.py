6db28b2f63b4fb07784ed8aa02e5cbb5de761779
Ryan Saul <rsaul@jamasoftware.com>
2015-07-21 11:47:57 -0700

    added content pages and content

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from collection.models import Hike
+
 
 # Create your views here.
 def index(request):
-    number = 6
-    return render(request, 'index.html', {'number': number})
+    hikes = Hike.objects.all().order_by('name')
+    return render(request, 'index.html', {'hikes': hikes}, )
+
+
+def hike_detail(request, slug):
+    hike = Hike.objects.get(slug=slug)
+    return render(request, 'hikes/hike_detail.html', {'hike': hike, })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^$','collection.views.index', name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'), 
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^hikes/(?P<slug>[-\w]+)/$', 'collection.views.hike_detail', name='hike_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
