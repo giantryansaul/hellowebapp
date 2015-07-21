09ad5da2e351ce6036427c763284124cb0eea887
Ryan Saul <rsaul@jamasoftware.com>
2015-07-20 19:56:16 -0700

    added admin panel

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from collection.models import Hike
+
+class HikeAdmin(admin.ModelAdmin):
+    model = Hike
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Hike, HikeAdmin)
