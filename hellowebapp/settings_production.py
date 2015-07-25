c5f5977ac8f4209be9f5ea5102c22378de669a1f
Ryan Saul <rsaul@jamasoftware.com>
2015-07-25 13:50:23 -0700

    changing production static files settings

--- hellowebapp/settings_production.py
+++ hellowebapp/settings_production.py
@@ -10,3 +10,7 @@ ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../collection/static'),
+)
