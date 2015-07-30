7ae1350c46dc7d3f97799792aba5a87ed33644f2
Ryan Saul <rsaul@jamasoftware.com>
2015-07-29 20:45:40 -0700

    set debug to true

--- hellowebapp/settings_production.py
+++ hellowebapp/settings_production.py
@@ -7,7 +7,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
