c325fd62a00983865a668e6cc6858ba576b743ba
Ryan Saul <rsaul@jamasoftware.com>
2015-07-31 15:33:58 -0700

    missing enough content on heroku site, setting debug back to false

--- hellowebapp/settings_production.py
+++ hellowebapp/settings_production.py
@@ -7,7 +7,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
+DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
